fcb465457e55707c6e79ec2f9452331595912d63
henry-dura <henryikemefuna@gmail.com>
2023-01-25 10:21:25 +0100

    confirm string  is between 1 and 10 characters

--- stringLength.js
+++ stringLength.js
@@ -1,5 +1,10 @@
 function stringLength(string){
-    return string.length;
+    if(string.length >= 1 && string.length<= 10){
+        return string.length;
+    }else{
+        return 'the string must be at least 1 character long and not longer than 10 characters';
+    }
+    
 }
 
 module.exports = stringLength;
