0e1cd0c3742c9470515cb12373abd11470597b2b
henry-dura <henryikemefuna@gmail.com>
2023-01-25 12:48:04 +0100

    write test for subtraction function

--- calculator.js
+++ calculator.js
@@ -1,5 +1,6 @@
 const calculator = {
     add: (num1,num2) => num1 +num2 ,
+    subtract: (num1,num2) => num1 - num2,
 }
 
 module.exports = calculator;
--- calculator.test.js
+++ calculator.test.js
@@ -3,5 +3,13 @@ const calculator = require('./calculator');
 describe('Check the calculator functions of add,subtract and multiply',()=>{
     test('add 4  plus 5 should be 9',()=>{
         expect(calculator.add(4,5)).toBe(9);
+    });
+
+    test('6 minus 2 should be 4',()=>{
+        expect(calculator.subtract(6,2)).toBe(4);
+    });
+    
+    test('2 minus 9 should be -7',()=>{
+        expect(calculator.subtract(2,9)).toBe(-7);
     })
 })
